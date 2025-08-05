700c81a8f185a532a55380c311dd7aac1b367d6c
PedrorFelix <pedrojrfelix1999@gmail.com>
2025-08-05 12:58:37 +0100

    mid-day break, adding navbar hamburger menu

--- src/pages/NavBar.js
+++ src/pages/NavBar.js
@@ -1,29 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
 import "./styles/NavBar.css";
 
 export function NavBar() {
-    const navigate = useNavigate();
-    const location = useLocation();
+  const navigate = useNavigate();
+  const location = useLocation();
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const toggleMenu = () =>{
+    setIsMenuOpen(!isMenuOpen);
+  };
 
-    return (
-        <nav className="NavBar">
-          {/* Navigation Buttons */}
+  const AutoCloseNav = (path) =>{
+    navigate(path);
+    setIsMenuOpen(false);
+  };
+
+  return (
+      <nav className="NavBar">
+        <div className="nav-menu">
           <button
           className={location.pathname ==="/" ? "active-tab" : "not-active-tab"} 
           onClick={() => navigate("/")}>Welcome</button>
-
           <button 
           className={location.pathname ==="/about" ? "active-tab" : "not-active-tab"} 
           onClick={() => navigate("/about")}>About</button>
-
           <button 
           className={location.pathname ==="/work" ? "active-tab" : "not-active-tab"} 
           onClick={() => navigate("/work")}>Work</button>
-
           <button 
           className={location.pathname ==="/contact" ? "active-tab" : "not-active-tab"} 
           onClick={() => navigate("/contact")}>Contact</button>
-        </nav>
-    );
+        </div>
+        
+      </nav>
+  );
 }
