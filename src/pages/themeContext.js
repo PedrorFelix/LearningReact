df45674c5850f4c4cfefdc6476fccf463990a77d
PedrorFelix <pedrojrfelix1999@gmail.com>
2025-08-19 19:35:36 +0100

    context and switcher for light and dark theme

---
+++ src/pages/themeContext.js
@@ -0,0 +1,48 @@
+import React, {createContext, useContext, useState, useEffect} from 'react;'
+
+//create context
+const ThemeContext = createContext();
+
+//create hook
+export const useTheme = () =>{
+    const context = useContext(ThemeContext);
+    return context;
+};
+
+export const ThemeProvider = ({ children }) => {
+    const [theme, setTheme] = useState( () => {
+        //if system uses dark theme
+        if(window.matchMedia && window.matchMedia('(prefers-color-scheme: dark').matches){
+            return 'dark';
+        }
+        //if not, deafult to light
+        return 'light';
+    });
+
+    useEffect(() =>{
+        //apply the new theme
+        document.documentElement.setAttribute('data-theme', theme);
+    }, [theme]);
+
+    const toggleTheme = () => {
+        //change themes
+        setTheme(prevTheme => prevTheme === 'light' ? 'dark' : 'light');
+    };
+
+    //everything needed to know about the theme
+    const value = {
+        theme, //current theme
+        toggleTheme, //theme switcher function
+        //bolleans to recognize dark or light
+        isDark: theme === 'dark',
+        isLight: theme === 'light'
+    };
+
+    return (
+        //this way all children will have access to the theme context
+        <ThemeContext.Provider value={value}>
+            {children}
+        </ThemeContext.Provider>
+    );
+
+};
---
+++ src/pages/themeSwitcher.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { useTheme } from "./themeContext";
+
+export function themeSwitcher() {
+    const { theme, toggleTheme } = useTheme();
+
+    return(
+        <button className="theme-switcher-btn"
+        onClick={toggleTheme}>
+            <span className='theme-selection'>
+                {theme ==='light' ? 'dark' : 'light'}
+            </span>
+        </button>
+    )
+}
