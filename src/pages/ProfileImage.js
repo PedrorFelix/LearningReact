66eab97fa62b603344f1f6f8e3c4592bf6ec3262
PedrorFelix <pedrojrfelix1999@gmail.com>
2025-08-20 13:12:58 +0100

    WIP changes to themes

--- src/pages/ProfileImage.js
+++ src/pages/ProfileImage.js
@@ -19,7 +19,7 @@ export function ProfileImage( { svgpath } ) {
         </defs>
         <rect x="0" y="0" width="540" height="540" fill="var(--bg-secondary)" mask="url(#maskForBg2)" />
 
-        <rect x="0" y="0" width="540" height="540" fill="var(--bg-accent)" mask="url(#maskForBg1)" />
+        <rect x="0" y="0" width="540" height="540" fill="var(--bg-primary)" mask="url(#maskForBg1)" />
         
         <image href= { imagePath } x="0" y="-25" width="540" height="540" mask="url(#maskForImage)" 
           alt="Pedro Félix - Junior Software Developer"/>
